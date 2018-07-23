3fa82db7f8b923a07e609ec7c7e24045b9ca5fee
Jonathan Pepler <jonathan.pepler@gmail.com>
2018-07-24 00:15:05 +0100

    change 1-1 to output base64

--- 1/1.rb
+++ 1/1.rb
@@ -1,3 +1,8 @@
-ARGV.each do |arg|
-  puts [arg].pack("H*")
+require 'base64'
+
+args = ARGV.empty? ? ["49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"] : ARGV
+
+args.each do |arg|
+  out = [arg].pack("H*")
+  puts Base64.encode64(out)
 end
