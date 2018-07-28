c318c875b752e4a5854ccabc06204f71bba97f9d
Jonathan Pepler <jonathan.pepler@gmail.com>
2018-07-28 23:57:24 +0100

    1-6: Calculate most likely keysize

--- 1/6.rb
+++ 1/6.rb
@@ -1,3 +1,11 @@
+require 'base64'
+require 'byebug'
+
+keysize_lower = 2
+keysize_upper = 40
+f = File.open("6.txt")
+input = Base64.decode64 f.read
+
 def hamming_distance(a,b)
   return -1 if a.length != b.length
   dist = 0
@@ -9,8 +17,34 @@ def hamming_distance(a,b)
   return dist
 end
 
+def hamming_distance_array(a,b)
+  return -1 if a.length != b.length
+  dist = 0
+  a.each_with_index do |a_char, index|
+    dist += 1 if a_char != b[index]
+  end
+  return dist
+end
+
 def to_bin(str)
   str.unpack("B*").first
 end
 
-puts hamming_distance "this is a test", "wokka wokka!!!"
+search = []
+(keysize_lower..keysize_upper).each do |keysize|
+  score = 0
+  count = 0
+  input.each_byte.each_slice(keysize).each_cons(2) do |a, b|
+    hd = hamming_distance_array a,b
+    score += hd / keysize
+    count += 1
+  end
+  search.push({ keysize: keysize, score: score.to_f / count.to_f })
+end
+
+search.sort! { |a,b|
+  a[:score] - b[:score]
+}
+puts "The most likely keysize is #{search.first[:keysize]}"
+# puts hamming_distance "this is a test", "wokka wokka!!!"
+
